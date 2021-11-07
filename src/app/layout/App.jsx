2a9e6091a009b3bdf77ffbf2166fa84e5a3fbf87
Ayush Vyas <sidharatvyas26@gmail.com>
2021-11-07 19:49:30 +0530

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import Navbar from '../../features/nav/Navbar';
+import {useState} from 'react';
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
-    <div className="App">
-    <h1>Re-vents</h1>
-    </div>
+    <>
+    <Navbar setFormOpen={setFormOpen}/>
+    <Container className="main">
+    <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+    </Container>
+    </>
   );
 }
 
---
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
+import EventListAttendee from "./EventListAttendee";
+
+export default function EventListItem({event}){
+    return(
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src={event.hostPhotoURL}></Item.Image>
+                        <Item.Content>
+                            <Item.Header content='Event Title'></Item.Header>
+                            <Item.Description>
+                                Hosted by {event.title}
+                            </Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock'></Icon>{event.date}
+                    <Icon name='marker'></Icon>{event.venue}
+                </span>
+            </Segment>
+            <Segment secondary>
+                <List horizontal>
+                    {event.attendees.map(attendee=>(
+                        <EventListAttendee key={attendee.id} attendee={attendee}/>
+                    ))}
+                </List>
+            </Segment>
+            <Segment clearing>
+                <span>{event.description}</span>
+                <Button color='teal' floated='right' content='View'></Button>
+            </Segment>
+        </Segment.Group>
+    )
+}
