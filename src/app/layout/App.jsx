af493720e8e9969897e57f9b435544a2b1b78c92
Ayush Vyas <sidharatvyas26@gmail.com>
2021-12-02 21:39:33 +0530

    End of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -6,12 +6,25 @@ import {useState} from 'react';
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event){
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen(){
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
 
   return (
     <>
-    <Navbar setFormOpen={setFormOpen}/>
+    <Navbar setFormOpen={handleCreateFormOpen}/>
     <Container className="main">
-    <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+    <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} 
+     selectEvent={handleSelectEvent} 
+     selectedEvent={selectedEvent}/>
     </Container>
     </>
   );
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem({event}){
+export default function EventListItem({event,selectEvent,deleteEvent}){
     return(
         <Segment.Group>
             <Segment>
@@ -33,7 +33,8 @@ export default function EventListItem({event}){
             </Segment>
             <Segment clearing>
                 <span>{event.description}</span>
-                <Button color='teal' floated='right' content='View'></Button>
+                <Button onClick={()=>deleteEvent(event.id)} color='red' floated='right' content='Delete'></Button>
+                <Button onClick={()=>selectEvent(event)} color='teal' floated='right' content='View'></Button>
             </Segment>
         </Segment.Group>
     )
